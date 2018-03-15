5159566e33acbe24be8d1e339534dcc6e7db21fb
DeaconLi <446995198@qq.com>
2018-03-15 22:48:11 +0800

    java-concurrency

---
+++ src/com/lizhuquan/concurrency/chapter6/_01ThreadInterrupt.java
@@ -0,0 +1,43 @@
+package com.lizhuquan.concurrency.chapter6;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 线程打断 和 优雅的关闭线程
+ */
+public class _01ThreadInterrupt {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t1 = new Thread(){
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println();
+                    System.out.println(">>t1 running on " + new SimpleDateFormat("yyyy/MM/dd hh:mm:ss").format(new Date()));
+                    try {
+                        Thread.sleep(100000);
+                    } catch (InterruptedException e) {
+                        System.out.println();
+                        System.out.println(">>线程t1被打断, 捕获异常后，继续往下执行...");
+                        System.out.println(">>线程状态：" + this.getState());
+                        System.out.println(">>线程中断状态：" + this.isInterrupted());
+                        System.out.println(">>线程是否存活：" + this.isAlive());
+                        // 可以通过break掉当前循环来优雅的结束线程
+                        break;
+                    }
+                }
+            }
+        };
+        t1.start();
+
+        while (true) {
+            Thread.sleep(2000);
+            System.out.println();
+            System.out.println("中断前t1的中断状态：" + t1.isInterrupted());
+            t1.interrupt();
+            System.out.println("中断后t1的中断状态：" + t1.isInterrupted());
+        }
+    }
+}
---
+++ src/com/lizhuquan/concurrency/chapter6/_02ThreadCloseForce.java
@@ -0,0 +1,69 @@
+package com.lizhuquan.concurrency.chapter6;
+
+/**
+ * 通过执行线程开启守护线程的方式，强制关闭长时间未工作完毕的守护线程
+ */
+public class _02ThreadCloseForce {
+
+    private static class TaskWrapper {
+        private Thread executeThread;
+        private boolean finish;
+
+        public void execute(Runnable task) {
+            executeThread = new Thread(){
+                @Override
+                public void run() {
+                    Thread workThread = new Thread(task);
+                    workThread.setDaemon(true);
+                    workThread.start();
+
+                    try {
+                        workThread.join();
+                        finish = true;
+                    } catch (InterruptedException e) {
+                        System.out.println(">> execute thread 被打断");
+                    }
+                }
+            };
+            executeThread.start();
+        }
+
+        public void shutdown(long mills) {
+            long currMills = System.currentTimeMillis();
+            while (!finish) {
+                if (System.currentTimeMillis() - currMills >= mills) {
+                    System.out.println(">> 任务超时，打断execute thread");
+                    finish = true;
+                    executeThread.interrupt();
+                    break;
+                }
+
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+            TaskWrapper taskWrapper = new TaskWrapper();
+            taskWrapper.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(">> 开始读取大文件");
+                    try {
+                        Thread.sleep(100_000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(">> 读取结束，完成任务");
+                }
+            });
+
+            long start = System.currentTimeMillis();
+            taskWrapper.shutdown(10_000);
+            System.out.println("总耗时：" + (System.currentTimeMillis() - start));
+        }
+    }
+}
