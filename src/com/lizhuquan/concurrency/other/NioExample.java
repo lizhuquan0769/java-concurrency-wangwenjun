e018a0378439b44e469a5261653f78709d68c722
DeaconLi <446995198@qq.com>
2018-05-12 23:00:08 +0800

    nio

--- src/com/lizhuquan/concurrency/other/NioExample.java
+++ src/com/lizhuquan/concurrency/other/NioExample.java
@@ -1,8 +1,6 @@
 package com.lizhuquan.concurrency.other;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -12,6 +10,29 @@ import java.nio.channels.FileChannel;
 public class NioExample {
 
     public static void main(String[] args) throws IOException {
-        
+        File file = new File("c:/spring-tool-suite-3.9.2.RELEASE-e4.7.2-win32-x86_64.zip");
+        FileInputStream fin = new FileInputStream(file);
+        FileChannel fcIn = fin.getChannel();
+        ByteBuffer byteBuff = ByteBuffer.allocate(1024 << 8);
+
+        FileOutputStream fout = new FileOutputStream(new File("d:/fout.out"));
+        FileChannel fcOut = fout.getChannel();
+
+        long b = System.currentTimeMillis();
+        while(fcIn.read(byteBuff) > -1) {
+            byteBuff.flip();
+            // 通过FileOutputStream写入
+            fout.write(byteBuff.array(), 0, byteBuff.limit());
+            // 通过FileChannel写入
+//            fcOut.write(byteBuff);
+            byteBuff.clear();
+        }
+        long e = System.currentTimeMillis();
+        System.out.println("span " + (e - b) + " ms");
+
+        fcIn.close();
+        fcOut.close();
+        fin.close();
+        fout.close();
     }
 }
